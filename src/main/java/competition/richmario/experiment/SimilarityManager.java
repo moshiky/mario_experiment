60a0bfd607bea6385db2f62fb0fbf251335f79ec
moshiky <moshec40@gmail.com>
2017-05-15 16:39:52 +0300

    * add similarity manager

---
+++ src/main/java/competition/richmario/experiment/SimilarityManager.java
@@ -0,0 +1,31 @@
+package competition.richmario.experiment;
+
+import org.apache.commons.math3.util.Pair;
+import java.util.ArrayList;
+import java.util.List;
+
+import competition.richmario.StateAction;
+
+/**
+ * Created by user on 15/05/2017.
+ */
+public class SimilarityManager {
+
+    /**
+     *
+     * @param state represents the state:
+     *              <to be completed>
+     * @param action represent the action:
+     *               <to be completed>
+     * @return
+     */
+    public static List<Pair<StateAction, Double>> getSimilarityRecords(int[] state, int action) {
+        ArrayList<Pair<StateAction, Double>> similarityRecords = new ArrayList<>();
+
+        // *** YOUR CODE HERE **********************************************************************
+
+        // *** END OF YOUR CODE ********************************************************************
+
+        return similarityRecords;
+    }
+}
