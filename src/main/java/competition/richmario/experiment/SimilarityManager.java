65929921a52bc2dd7bc75fec51256da72c9c40c3
moshiky <moshec40@gmail.com>
2017-06-19 17:13:53 +0300

    * code completed

--- src/main/java/competition/richmario/experiment/SimilarityManager.java
+++ src/main/java/competition/richmario/experiment/SimilarityManager.java
@@ -98,6 +98,67 @@ public class SimilarityManager {
 
         // *** YOUR CODE HERE **********************************************************************
 
+        int[] state_0 = {0,1};
+        int[] state_1 = {0,1};
+        int[] state_2 = {0,1};
+        int[] state_3 = {0,1,2,3,4,5,6,7,8};
+        int[] state_4 = {1,2,4,8,16,32,64,128};
+        int[] state_5 = {1,2,4,8,16,32,64,128};
+        int[] state_6 = {1,2,4,8,16,32,64,128};
+        int[] state_7 = {1,2,4,8};
+        int[] action_8 = {0,1,2,3,4,5,6,7,8,9,10,11};
+
+        //int[] newState = state.clone();
+        //newState[4] = 10;
+
+        for(int i_0=0; i_0<state_0.length;i_0++){
+            for(int i_1=0; i_1<state_1.length;i_1++){
+                for(int i_2=0; i_2<state_2.length;i_2++){
+                    for(int i_3=0; i_3<state_3.length;i_3++){
+                        for(int i_4=0; i_4<state_4.length;i_4++){
+                            for(int i_5=0; i_5<state_5.length;i_5++){
+                                for(int i_6=0; i_6<state_6.length;i_6++){
+                                    for(int i_7=0; i_7<state_7.length;i_7++){
+                                        for(int i_8=0; i_8<action_8.length;i_8++){
+
+                                            int[] newState = state.clone();
+                                            int newAction;
+                                            int count=0;
+                                            double factor=0.0;
+
+                                            newState[0] = state_0[i_0];
+                                            newState[1] = state_1[i_1];
+                                            newState[2] = state_2[i_2];
+                                            newState[3] = state_3[i_3];
+                                            newState[4] = state_4[i_4];
+                                            newState[5] = state_5[i_5];
+                                            newState[6] = state_6[i_6];
+                                            newState[7] = state_7[i_7];
+                                            newAction = action_8[i_8];
+
+                                            for(int i=0;i<8;i++){
+                                                if(state[i] == newState[i]){count+=1;}
+                                            }
+                                            if(action == newAction){count+=1;}
+
+                                            factor = count/9;
+
+                                            similarityRecords.add(new Pair<StateAction, Double>(new StateAction(newState, newAction), factor));
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+
+
+
+        //similarityRecords.add(new Pair<StateAction, Double>(new StateAction(newState, newAction), factor));
+
         // *** END OF YOUR CODE ********************************************************************
 
         return similarityRecords;
@@ -107,4 +168,5 @@ public class SimilarityManager {
     // Here you can add custom STATIC help functions, if needed
 
     // *** END OF YOUR CODE ********************************************************************
+
 }
